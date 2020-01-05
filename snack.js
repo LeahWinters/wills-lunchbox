4c40040a891bcb0e3c3f7933268248770473451f
Leah Winters <leah.winters24@gmail.com>
2020-01-05 13:21:01 -0700

    Pass all tests for snacks

--- snack.js
+++ snack.js
@@ -1,4 +1,23 @@
 class Snack {
+  constructor(type){
+    this.deliciousLevel = "extra";
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.healthy = this.checkIfHealthy();
+  }
+  getEaten() {
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+
+checkIfHealthy() {
+  var words = this.type.split(" ");
+  return words.some(word => word === "Fruit")
+}
 
 }
 
